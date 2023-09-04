6b9bfaeb2791d7bb99e337f0b42306bf1347f7f2
Jean Gabin Ishimwe <ij.gabin123@gmail.com>
2023-09-04 11:32:18 +0200

    errors

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     header('Access-Control-Allow-Origin: *');
