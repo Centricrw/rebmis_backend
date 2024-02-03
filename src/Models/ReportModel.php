3099f079bf22b5ab1793374f3a0397f483a18082
clement <muhirwaclement@gmail.com>
2024-02-03 12:27:55 +0200

    added some markings

--- src/Models/ReportModel.php
+++ src/Models/ReportModel.php
@@ -11,14 +11,29 @@ class ReportModel {
     }
 
     public function getGeneralReport()
-    {
-        
+    { 
       $statement = " SELECT * FROM general_report";
-
       try {
           $statement = $this->db->query($statement);
           $result = $statement->fetchAll(\PDO::FETCH_ASSOC);
           return $result;
+      } catch (\PDOException $e) {
+          exit($e->getMessage());
+      }
+    }
+    public function markTheTrainee($data)
+    {  
+      $markType = $data['markType'];
+      $statement = "UPDATE general_report SET $markType = :marks WHERE userId = :userId AND unitId = :unitId AND cohortId = :cohortId";
+      try {
+          $statement = $this->db->prepare($statement);
+            $statement->execute(array(
+                ":userId" => $data['userId'],
+                ":cohortId" => $data['cohortId'],
+                ":unitId" => $data['unitId'],
+                ":marks" => $data['marks'],
+            ));
+            return $statement->rowCount();
       } catch (\PDOException $e) {
           exit($e->getMessage());
       }
