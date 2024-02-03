1b224c0533f95d808f15aa900f96adfce47f2397
clement <muhirwaclement@gmail.com>
2024-02-03 12:27:55 +0200

    added some markings

--- src/Controller/reportController.php
+++ src/Controller/reportController.php
@@ -28,7 +28,9 @@
                 }
                 break;
               case "POST":
-                $response = 'testing post';
+                if (sizeof($this->params) > 0 && $this->params['action'] == "mark") {
+                  $response = $this->markTheTrainee();
+                }
                 break;
               default:
                 $response = Errors::notFoundError('plan not found');
@@ -42,11 +44,27 @@
 
     private function getGeneralReport()
     {   
-            $result = $this->reportModel->getGeneralReport();
-            $response['status_code_header'] = 'HTTP/1.1 200 OK';
-            $response['body'] = json_encode($result);
-            return $response;
+      $result = $this->reportModel->getGeneralReport();
+      $response['status_code_header'] = 'HTTP/1.1 200 OK';
+      $response['body'] = json_encode($result);
+      return $response;
     }
+    private function markTheTrainee()
+    {
+      // getting input data
+      $inputData = (array) json_decode(file_get_contents('php://input'), true);
+
+      try {
+          $result = $this->reportModel->markTheTrainee($inputData);
+          // response
+          $response['status_code_header'] = 'HTTP/1.1 201 Created';
+          $response['body'] = json_encode($result);
+          return $response;
+      }
+      catch (\Throwable $th) {
+          return Errors::databaseError($th->getMessage());
+      }
+  }
 }
 $controller = new reportController($this->db, $request_method, $params);
 $controller->processRequest();
