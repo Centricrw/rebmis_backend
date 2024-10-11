276ae172c65e933ee92fabc6857794fb1282eab7
Clement <muhirwaclement@gmail..com>
2024-10-11 06:48:16 +0200

    added all enrollments with nodes ids

--- src/Controller/livemoodleController.php
+++ src/Controller/livemoodleController.php
@@ -80,15 +80,15 @@ class livemoodleController
     {
         $result = [];
         try {
-            $all = new \stdClass; $all->courseId = "0"; $all->enrolled = "All: 0";
-            $test = new \stdClass; $test->courseId = "889"; $test->enrolled = "PA Test: ".$this->livemoodleModel->get_enrollments("889");
-            $bl1 = new \stdClass; $bl1->courseId = "782"; $bl1->enrolled = "BL1: ".$this->livemoodleModel->get_enrollments("782");
-            $bl2 = new \stdClass; $bl2->courseId = "784"; $bl2->enrolled = "BL2: ".$this->livemoodleModel->get_enrollments("784");
-            $bl3 = new \stdClass; $bl3->courseId = "489"; $bl3->enrolled = "BL3: ".$this->livemoodleModel->get_enrollments("489");
-            $il1 = new \stdClass; $il1->courseId = "788"; $il1->enrolled = "IL1: ".$this->livemoodleModel->get_enrollments("788");
-            $il2 = new \stdClass; $il2->courseId = "789"; $il2->enrolled = "IL2: ".$this->livemoodleModel->get_enrollments("789");
-            $il3 = new \stdClass; $il3->courseId = "790"; $il3->enrolled = "IL3: ".$this->livemoodleModel->get_enrollments("790");
-            $certified = new \stdClass; $certified->courseId = "00"; $certified->enrolled = "Certified: 0";
+            $all = new \stdClass; $all->id = "1"; $all->courseId = "0"; $all->enrolled = "All: 0";
+            $test = new \stdClass; $test->id = "2"; $test->courseId = "889"; $test->enrolled = "PA Test: ".$this->livemoodleModel->get_enrollments("889");
+            $bl1 = new \stdClass; $bl1->id = "3"; $bl1->courseId = "782"; $bl1->enrolled = "BL1: ".$this->livemoodleModel->get_enrollments("782");
+            $bl2 = new \stdClass; $bl2->id = "4"; $bl2->courseId = "784"; $bl2->enrolled = "BL2: ".$this->livemoodleModel->get_enrollments("784");
+            $bl3 = new \stdClass; $bl3->id = "5"; $bl3->courseId = "489"; $bl3->enrolled = "BL3: ".$this->livemoodleModel->get_enrollments("489");
+            $il1 = new \stdClass; $il1->id = "6"; $il1->courseId = "788"; $il1->enrolled = "IL1: ".$this->livemoodleModel->get_enrollments("788");
+            $il2 = new \stdClass; $il2->id = "7"; $il2->courseId = "789"; $il2->enrolled = "IL2: ".$this->livemoodleModel->get_enrollments("789");
+            $il3 = new \stdClass; $il3->id = "8"; $il3->courseId = "790"; $il3->enrolled = "IL3: ".$this->livemoodleModel->get_enrollments("790");
+            $certified = new \stdClass; $certified->id = "8"; $certified->courseId = "00"; $certified->enrolled = "Certified: 0";
 
             array_push($result,$test,$bl1,$bl2,$bl3,$il1,$il2,$il3,$certified);
             //$result = $this->livemoodleModel->get_enrollments($courseId);
