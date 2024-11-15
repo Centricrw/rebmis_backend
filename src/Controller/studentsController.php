d6586cc646d5558453280815cb75f4fb01cf6320
Jean Gabin Ishimwe <ij.gabin123@gmail.com>
2024-11-15 14:32:38 +0200

    path added

--- src/Controller/studentsController.php
+++ src/Controller/studentsController.php
@@ -206,7 +206,9 @@ class BrandsController
         $logged_user_id = AuthValidation::authorized()->id;
         try {
             // initializing socket
-            $socketVersion = new Version2X("http://localhost:6200");
+            $socketVersion = new Version2X("http://localhost:6200", [
+                'path' => '/rebmis_live',
+            ]);
             $socketClient = new Client($socketVersion);
             $socketClient->initialize();
 
