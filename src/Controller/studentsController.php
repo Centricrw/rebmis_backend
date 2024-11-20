3e2f903f7f566137539a5ed2711b45f3ce40b352
Jean Gabin Ishimwe <ij.gabin123@gmail.com>
2024-11-20 10:54:11 +0200

    path removed

--- src/Controller/studentsController.php
+++ src/Controller/studentsController.php
@@ -207,7 +207,7 @@ class BrandsController
         $logged_user_id = AuthValidation::authorized()->id;
         try {
             // initializing socket
-            $socketVersion = new Version2X("http://localhost:6200/rebmis_live");
+            $socketVersion = new Version2X("http://localhost:6200");
             $socketClient = new Client($socketVersion);
             $socketClient->initialize();
 
